ca6eb62f02b3dbdf933cfec7db44ff4aa484690c
kazlan <jorge.mollon@gmail.com>
2016-10-30 09:28:38 +0100

    sprint 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpModule } from '@angular/http'
+
+import { MaterialModule } from '@angular/material'
+
+import { StoreModule } from '@ngrx/store'
+import { listaReducer } from './reducers/lista.reducer'
 
 import { AppComponent } from './app.component';
+import { GroceriesComponent } from './groceries/groceries.component';
+import { ListaComponent } from './lista/lista.component';
+import { NewListFormComponent } from './new-list-form/new-list-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GroceriesComponent,
+    ListaComponent,
+    NewListFormComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule.forRoot(),
+    StoreModule.provideStore({
+      listas: listaReducer
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
